2f54fa8695f5a68a8f7f15209d490e7d4d8962ce
aungkokodev <aungkokoepic@gmail.com>
2026-06-26 15:15:17 +0630

    Exercise 15

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,12 +3,51 @@
 import { db } from '@/db'
 import { users } from '@/db/schema'
 import bcrypt from 'bcryptjs'
+import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
 
-export const registerUser = async (formData: FormData) => {
+const validate = (value: string, name: string, len = 4) => {
+  if (!value) return `${name} must not be empty`
+  else if (value.length < len)
+    return `${name} must be at least ${len} characters`
+  return ''
+}
+
+interface FormState {
+  name: string
+  username: string
+  password: string
+  confirm: string
+}
+
+export const registerUser = async (
+  prevState: { errors: FormState; values: FormState },
+  formData: FormData
+) => {
   const name = (formData.get('name') as string).trim()
   const username = (formData.get('username') as string).trim()
   const password = formData.get('password') as string
+  const confirm = formData.get('confirm') as string
+
+  const errors = {
+    name: validate(name, 'name'),
+    username: validate(username, 'username'),
+    password: validate(password, 'password', 0),
+    confirm: password !== confirm ? 'passwords do not match' : ''
+  }
+
+  if (Object.values(errors).some(Boolean)) {
+    return { errors, values: { name, username, password, confirm } }
+  }
+
+  const userExists = await db.query.users.findFirst({
+    where: eq(users.username, username)
+  })
+
+  if (userExists) {
+    errors.username = 'username must be unique'
+    return { errors, values: { name, username, password, confirm } }
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,39 +1,76 @@
+'use client'
+
+import { useActionState } from 'react'
 import { registerUser } from '../actions/users'
 
+const initialState = {
+  name: '',
+  username: '',
+  password: '',
+  confirm: ''
+}
+
 const Register = () => {
+  const [state, formAction] = useActionState(registerUser, {
+    errors: { ...initialState },
+    values: { ...initialState }
+  })
+
   return (
     <div>
       <h2>Regiser</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
-            name
+            Name
             <input
               type='text'
               name='name'
-              required
+              defaultValue={state.values?.name}
             />
           </label>
+          {state.errors?.name && (
+            <span style={{ color: 'red' }}> {state.errors?.name}</span>
+          )}
         </div>
         <div>
           <label>
-            username
+            Username
             <input
               type='text'
               name='username'
-              required
+              defaultValue={state.values?.username}
             />
           </label>
+          {state.errors?.username && (
+            <span style={{ color: 'red' }}> {state.errors?.username}</span>
+          )}
         </div>
         <div>
           <label>
-            password
+            Password
             <input
               type='password'
               name='password'
-              required
+              defaultValue={state.values?.password}
             />
           </label>
+          {state.errors?.password && (
+            <span style={{ color: 'red' }}> {state.errors?.password}</span>
+          )}
+        </div>
+        <div>
+          <label>
+            Confirm Password
+            <input
+              type='password'
+              name='confirm'
+              defaultValue={state.values?.confirm}
+            />
+          </label>
+          {state.errors?.confirm && (
+            <span style={{ color: 'red' }}> {state.errors?.confirm}</span>
+          )}
         </div>
         <button type='submit'>Register</button>
       </form>
